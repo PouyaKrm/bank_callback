65a9570fdb3ebebb08a085f688ec90184656bc1b
Pouya <pouya795@gmail.com>
2026-09-14 11:54:24 +0330

    Seller model

--- order/models.py
+++ order/models.py
@@ -5,3 +5,9 @@ class Order(models.Model):
     """An order with an automatically generated primary key."""
 
     name = models.CharField(max_length=250)
+
+
+class Seller(models.Model):
+    """Seller account information."""
+
+    balance = models.BigIntegerField(default=0)
