c1ccfd7d0978a80aded4f76532168d8a17b6b9f2
Pouya <pouya795@gmail.com>
2026-09-14 11:58:17 +0330

    order relationship

--- order/models.py
+++ order/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 
-class Order(models.Model):
-    """An order with an automatically generated primary key."""
-
-    name = models.CharField(max_length=250)
-
-
 class Seller(models.Model):
     """Seller account information."""
 
     balance = models.BigIntegerField(default=0)
+
+
+class Order(models.Model):
+    """An order with an automatically generated primary key."""
+
+    name = models.CharField(max_length=250)
+    seller = models.ForeignKey('Seller', on_delete=models.RESTRICT)
