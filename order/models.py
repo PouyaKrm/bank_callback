04aa1e4693bec4bc29dc0bbbe0ff519237025ddb
Pouya <pouya795@gmail.com>
2026-09-14 10:18:01 +0330

    Adds the payment and orders apps

---
+++ order/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Order(models.Model):
+    """An order with an automatically generated primary key."""
---
+++ payment/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+
+class Payment(models.Model):
+    """A payment with an automatically generated primary key."""
